ed62c4dce5fca7dc0e4196189cf9f61c26988e36
web4sumit <web4sumit@gmail.com>
2023-07-06 17:38:25 +0530

    divided file

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function createIframe(url, width, height){
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute("width", width);
+    iframe.setAttribute("height", height);
+    iframe.setAttribute("src", url);
+    return iframe;
+} 
+
+
+function generate_iframe() { 
+
+    const base_url = document.getElementById("url").value;
+    const number = document.getElementById("totalNumber").value;
+    const post_url = base_url.replace("watch?v=", "embed/");
+    const URL = post_url + "?autoplay=1&mute=1"
+    const wrapper = document.getElementById("iframe_wrapper");
+    wrapper.innerHTML = '';
+
+    for (let i = 0; i < number; i++) {
+        // const element = array[index];    
+        const iframe_child = createIframe(URL, 320, 300)
+        wrapper.appendChild(iframe_child)
+        
+    }
+
+}
+        // const BASEURL = 'https://www.youtube.com/embed/oItJut-ruRk';
+        // const LASTURL = '?autoplay=1&mute=1';
+        // const URL = document.getElementById('url');
+        // const button = document.getElementById('submitplay');
+        // const incrementCount = document.getElementById('totalNumber');
+        // const myframe = document.getElementById('myframe');
+        // button.addEventListener('click',()=>{
+        //     const inputVal = URL.value;
+        //     const totalNum = incrementCount.value;
+        //     // if(inputVal == "") return;
+        //     const dynamicSrc = `${BASEURL}${LASTURL}`;
+        //     myframe.src = dynamicSrc;
+        //     console.log(dynamicSrc,totalNum);
+        // })
